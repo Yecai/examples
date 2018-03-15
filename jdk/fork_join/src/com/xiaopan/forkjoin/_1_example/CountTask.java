525a9b16a6a5c939170299ccf9cb3e8d018d0475
panhuaxiong <panhuaxiong@jd.com>
2018-03-15 15:19:34 +0800

    fork/join

---
+++ jdk/fork_join/src/com/xiaopan/forkjoin/_1_example/CountTask.java
@@ -0,0 +1,67 @@
+package com.xiaopan.forkjoin._1_example;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+/**
+ * @Package: com.xiaopan.forkjoin._1_example
+ * @ClassName: CountTask
+ * @author: panhuaxiong
+ * @date: 2017年8月18日 下午4:09:56
+ * @Description: 使用Fork／Join框架,计算1+2+3+4的结果
+ * http://www.infoq.com/cn/articles/fork-join-introduction
+ */
+@SuppressWarnings("serial")
+public class CountTask extends RecursiveTask<Integer> {
+	private static final int THRESHOLD = 2; //阀值
+	private int start;
+	private int end;
+
+	public CountTask(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public static void main(String[] args) {
+		ForkJoinPool forkJoinPool = new ForkJoinPool();
+		//生成一个计算任务，负责计算1+2+3+4
+		CountTask task = new CountTask(1, 4);
+		//执行一个任务
+		Future<Integer> result = forkJoinPool.submit(task);
+		try {
+			System.out.println(result.get());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected Integer compute() {
+		int sum = 0;
+		
+		//如果任务足够小就计算任务
+		boolean canCompute = (end - start) < THRESHOLD;
+		if (canCompute) {
+			for (int i = start; i < end; i++) {
+				sum += i;
+			}
+		} else {
+			//如果任务大于阀值，就分裂成两个子任务计算
+			int middle = (start + end) / 2;
+			CountTask leftTask = new CountTask(start, middle);
+			CountTask rightTask = new CountTask(middle + 1, end);
+			//执行子任务
+			leftTask.fork();
+			rightTask.fork();
+			
+			//等待子任务执行完，并得到其结果
+			int leftResult = leftTask.join();
+			int rightResult = rightTask.join();
+			//合并子任务
+			sum = leftResult + rightResult;
+		}
+		return sum;
+	}
+
+}
