d79be3650fe039c41eadbe9b5926eae0ef6a7ac6
Yecai <pan2624@163.com>
2019-03-15 14:54:58 +0800

    spring integration helloWorld demo

---
+++ spring/spring-integration/s-i-helloworld/README.md
@@ -0,0 +1,12 @@
+Hello World示例
+==================
+
+根据spring Integration官方simples写的示例
+主要用于学习和后期回顾参考
+
+
+此示例包含内容
+* Hello World输出
+* 拉取消息
+
+
---
+++ spring/spring-integration/s-i-helloworld/src/main/java/com/xiaopan/HelloService.java
@@ -0,0 +1,11 @@
+package com.xiaopan;
+
+/**
+ * 信息处理
+ */
+public class HelloService {
+
+	public String sayHello(String name) {
+		return "Hello, " + name;
+	}
+}
